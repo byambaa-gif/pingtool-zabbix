58b73028067474ee6a6070d555c68a0456281857
Byamba-Erdene Namjilsuren <byambaerdene@mobicom.mn>
2023-12-12 17:04:53 +0900

    read excel for every ip

--- excelimport/urls.py
+++ excelimport/urls.py
@@ -4,5 +4,4 @@ from .views import upload_excel
 
 urlpatterns = [
     path('upload/', upload_excel, name='upload_excel'),
-
 ]
--- excelimport/views.py
+++ excelimport/views.py
@@ -5,22 +5,26 @@ from .forms import ExcelUploadForm
 from .models import ExcelData
 import pandas as pd
 import json
+from excelimport import excel_to_zabbix_service
+
 
 def upload_excel(request):
     if request.method == 'POST':
         form = ExcelUploadForm(request.POST, request.FILES)
         if form.is_valid():
             excel_file = request.FILES['file']
-            df = pd.read_excel(excel_file)
-            data_dict = df.to_dict(orient='records')
+            # df = pd.read_excel(excel_file)
             
-            # Ensure valid JSON
-            json_data = json.dumps(data_dict)
+            return excel_to_zabbix_service.process_first_sheet_for_work_name_sheet_number(excel_file)
+           
+            # data_dict = df.to_dict(orient='records')
+            # # Ensure valid JSON
+            # json_data = json.dumps(data_dict)
 
-            # Create the ExcelData object
-            ExcelData.objects.create(data=json_data)
+            # # Create the ExcelData object
+            # ExcelData.objects.create(data=json_data)
 
-            return JsonResponse(data_dict, safe=False)
+            # return JsonResponse(data_dict, safe=False)
     else:
         form = ExcelUploadForm()
     return render(request, 'upload.html', {'form': form})
